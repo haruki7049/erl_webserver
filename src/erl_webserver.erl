39c4422f9caf60eaf401d1ff9d9e616625af8dd1
haruki7049 <tontonkirikiri@gmail.com>
2024-09-11 13:56:08 +0900

    feat(erl_webserver): Edit

--- src/erl_webserver.erl
+++ src/erl_webserver.erl
@@ -1,3 +1,9 @@
 -module(erl_webserver).
 
--export([]).
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+	elli:start_link([{callback, elli_example_callback}, {port, 3000}]).
+
+stop(_State) ->
+	ok.
